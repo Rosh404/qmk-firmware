35f60938f1d889431e9467dfefd9851aff2ec494
rosh <roshandx88@gmail.com>
2026-09-03 00:54:13 +0800

    - update caps word config

--- keymap.c
+++ keymap.c
@@ -374,6 +374,7 @@ bool caps_word_press_user(uint16_t keycode) {
             return true;
         // Keycodes that continue Caps Word, without shifting.
         case KC_MINS:
+        case KC_DOT:
         case KC_1 ... KC_0:
         case KC_BSPC:
         case KC_DEL:
